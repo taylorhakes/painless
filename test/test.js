419cfbc097cb584ab445d692f1e584b3b2dc1037
taylorhakes <taylor@taylorhakes.com>
2015-11-30 00:10:33 -0500

    Added tests for Observable, Promise and Streams

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
 var tap = require('tap')
 var assert = require('chai').assert;
 var Test = require('../lib/test');
+var Promise  = require('promise-polyfill');
+var Observable = require("zen-observable");
+var through = require('through');
 var test = tap.test;
 var noop = function() {};
 
@@ -96,11 +99,178 @@ test('failing test', function(t) {
 test('failing test callback', function(t) {
   var tes = Test('hello', function(done) {
     setTimeout(function() {
+      assert.equal(1, 2);
+      done();
+    }, 10);
+  });
+  tes.on('end', function(val) {
+    t.is(val.ok, false);
+    t.is(val.actual, 1);
+    t.is(val.expected, 2);
+    t.end();
+  });
+  tes.run();
+});
+
+test('failing test callback with error', function(t) {
+  var tes = Test('hello', function(done) {
+    setTimeout(function() {
       done(new Error());
     }, 10);
   });
   tes.on('end', function(val) {
     t.is(val.ok, false);
+    t.end();
+  });
+  tes.run();
+});
+
+test('passing callback', function(t) {
+  var tes = Test('hello', function(done) {
+    setTimeout(function() {
+      assert(true);
+      done();
+    }, 10);
+  });
+  tes.on('end', function(val) {
+    t.is(val.ok, true);
+    t.end();
+  });
+  tes.run();
+});
+
+test('failing Promise', function(t) {
+  var tes = Test('hello', function() {
+    return Promise.resolve(10).then(function() {
+      assert.equal(1, 2);
+    });
+  });
+  tes.on('end', function(val) {
+    t.is(val.ok, false);
+    t.is(val.actual, 1);
+    t.is(val.expected, 2);
+    t.end();
+  });
+  tes.run();
+});
+
+test('fail before Promise created', function(t) {
+  var tes = Test('hello', function() {
+    assert.equal(1, 2);
+    return Promise.resolve(10);
+  });
+  tes.on('end', function(val) {
+    t.is(val.ok, false);
+    t.is(val.actual, 1);
+    t.is(val.expected, 2);
+    t.end();
+  });
+  tes.run();
+});
+
+test('passing Promise', function(t) {
+  var value;
+  var tes = Test('hello', function() {
+    return Promise.resolve(10).then(function() {
+      assert(true);
+      value = 1;
+    });
+  });
+  tes.on('end', function(val) {
+    t.is(val.ok, true);
+    t.is(value, 1);
+    t.end();
+  });
+  tes.run();
+});
+
+test('failing Observable', function(t) {
+  var tes = Test('test failing observable', function() {
+    return new Observable(function(observer) {
+      observer.next(8);
+      observer.next(9);
+
+      // Failing assert
+      assert.equal(1, 2);
+      observer.complete();
+    });
+  });
+  tes.on('end', function(val) {
+    t.is(val.ok, false);
+    t.is(val.actual, 1);
+    t.is(val.expected, 2);
+    t.end();
+  });
+  tes.run();
+});
+
+test('failing Observable map', function(t) {
+  var tes = Test('test failing observable', function() {
+    return Observable.of([1,2,3]).map(function(val) {
+      assert.equal(3, 4);
+      return 1;
+    });
+  });
+  tes.on('end', function(val) {
+    t.is(val.ok, false);
+    t.is(val.actual, 3);
+    t.is(val.expected, 4);
+    t.end();
+  });
+  tes.run();
+});
+
+
+test('passing observable', function(t) {
+  var tes = Test('test failing observable', function() {
+    return new Observable(function(observer) {
+      observer.next(8);
+      observer.next(9);
+
+      // Passing assert
+      assert.equal(2, 2);
+      observer.complete();
+    });
+  });
+  tes.on('end', function(val) {
+    t.is(val.ok, true);
+    t.end();
+  });
+  tes.run();
+});
+
+test('failing stream', function(t) {
+  var tes = Test('test failing observable', function() {
+    var stream = through();
+    setTimeout(function() {
+      stream.emit('error', new Error('invalid stream'));
+      stream.destroy();
+    }, 10);
+
+    return stream;
+  });
+  tes.on('end', function(val) {
+    t.is(val.ok, false);
+    t.is(val.error.message, 'invalid stream');
+    t.end();
+  });
+  tes.run();
+});
+
+test('passing stream', function(t) {
+  var tes = Test('test failing observable', function() {
+    var stream = through();
+    setTimeout(function() {
+      stream.queue(1);
+      stream.queue(null);
+      stream.destroy();
+    }, 10);
+
+
+    return stream;
+  });
+  tes.on('end', function(val) {
+    t.is(val.ok, true);
     t.end();
   });
   tes.run();
