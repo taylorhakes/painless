841786c89a99e98f56be04a850592294280b7eb5
taylorhakes <taylor@taylorhakes.com>
2016-02-04 01:11:02 -0500

    Fixed eslint errors

--- index.js
+++ index.js
@@ -5,24 +5,22 @@ var sinon = require('sinon');
 var tapSpec = require('tap-spec');
 var createHarness = require('./lib/create-harness');
 
-var start = Date.now();
 var harness = createHarness();
 
-setAsap(function() {
+setAsap(function asap() {
   var groupOutput = harness.run();
   var processOutput = tap(groupOutput).pipe(tapSpec());
   var hasError = false;
-  groupOutput.on('data', function(info) {
+  groupOutput.on('data', function onData(info) {
     if (!info.success) {
       hasError = true;
     }
   });
-  groupOutput.on('end', function() {
-    console.log(Date.now() - start);
+  groupOutput.on('end', function onEnd() {
     process.exit(hasError ? 1 : 0);
   });
   processOutput.pipe(process.stdout);
-  process.on('exit', function(code) {
+  process.on('exit', function onExit(code) {
     if (code || !hasError) {
       return;
     }
